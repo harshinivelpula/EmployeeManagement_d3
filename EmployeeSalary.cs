a21340fbcb869e72c4b1403eab50cc69feeae387
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:22:45 +0530

    check employee is present or absent

---
+++ EmployeeSalary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement1
+{
+    public class EmployeeSalary
+    {
+        public void EmployeeAttendance()
+        {
+            //constants
+            int IS_FUL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FUL_TIME)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
