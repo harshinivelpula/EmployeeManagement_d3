7164245b3d210d15a2bfe30c24c16a8e92e02c92
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:15:21 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
