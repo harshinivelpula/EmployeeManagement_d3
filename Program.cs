57002b19b935065173eb550c411f4d45c5ea3e84
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:32:23 +0530

    cal daily emp wage

--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace EmployeeManagement1
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmployeeSalary EmployeeSalary = new EmployeeSalary();
-            EmployeeSalary.EmployeeAttendance();
+            //EmployeeSalary EmployeeSalary = new EmployeeSalary();
+            //EmployeeSalary.EmployeeAttendance();
+            //Console.ReadKey();
+            DailyEmployeeWage dailyEmployeeWage = new DailyEmployeeWage();
+            DailyEmployeeWage.DailyWage();
             Console.ReadKey();
         }
     }
