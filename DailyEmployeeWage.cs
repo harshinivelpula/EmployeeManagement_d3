7000e3a73f46a2249a5750b4917950398aa81e61
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:26:48 +0530

    cal daily emp wage

---
+++ DailyEmployeeWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement1
+{
+    internal class DailyEmployeeWage
+    {
+        public static void DailyWage()
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empcheck = random.Next(2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp wage:" + empwage);
+        }
+    }
+}
