f0f3af0f4d9498c3393990fa2bebacaf0499f18a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:23:53 +0530

    check employee is present or absent

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace EmployeeManagement
+﻿using EmployeeManagement1;
+
+namespace EmployeeManagement1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeSalary EmployeeSalary = new EmployeeSalary();
+            EmployeeSalary.EmployeeAttendance();
+            Console.ReadKey();
         }
     }
 }
