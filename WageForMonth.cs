1fcc03800974cbb40c0b745f1acc31cb05b10c1f
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-22 12:49:12 +0530

    cal wages for month

---
+++ WageForMonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement1
+{
+    internal class WageForMonth
+    {
+        public static void MonthlyWage()
+        {
+            //Constants
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
+            const int NUM_OF_WORKING_DAYS = 2;
+            //Variables
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage:" + empWage);
+            }
+            Console.WriteLine("total emp wage:" + totalEmpWage);
+        }
+    }
+}
